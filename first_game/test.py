a4c63954f17cd29b9b50e761d1d30980282e3fc5
KrystianMank <man-kry-2005@wp.pl>
2024-02-01 01:55:50 +0100

    kolory

--- first_game/test.py
+++ first_game/test.py
@@ -17,7 +17,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 #utworzenie powierzchni
 sky_surface = pygame.image.load('images/Sky.png').convert()
 ground_surface = pygame.image.load('images/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surface = test_font.render('My game', False, (64,64,64))
+score_rect = score_surface.get_rect(center = (400,50))
 
 snail_surface = pygame.image.load('images/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (600,300))
@@ -38,7 +40,9 @@ while True:
     #umieszczenie powierzchni na okno główne
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+    screen.blit(score_surface,score_rect)
     
     snail_rect.left -= 4
     if snail_rect.left < -100:
