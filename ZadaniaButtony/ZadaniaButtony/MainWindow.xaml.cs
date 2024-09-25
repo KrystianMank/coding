8dae0fa44de11cab8847db25f386c5abd343d0f2
KrystianMank <man-kry-2005@wp.pl>
2024-09-25 21:34:26 +0200

    Zadania buttony

--- ZadaniaButtony/ZadaniaButtony/MainWindow.xaml.cs
+++ ZadaniaButtony/ZadaniaButtony/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -31,10 +32,31 @@ namespace ZadaniaButtony
             btnPowieksz.Height += btnPowieksz.Height * 0.2;
 
         }
-
+        bool change = false;
         private void btnZablokuj_Click(object sender, RoutedEventArgs e)
         {
+            Button[] buttons = { btnPowieksz, button1, button2 };
+
+            foreach (Button button in buttons)
+            {
+                button.IsEnabled = change; 
+            }
+
+            change = !change; 
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            button1.Visibility = Visibility.Hidden;
+            button2.Visibility = Visibility.Visible;
             
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            button1.Visibility = Visibility.Visible;
+            button2.Visibility = Visibility.Hidden;
         }
     }
 }
